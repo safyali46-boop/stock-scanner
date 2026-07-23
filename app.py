0cdec78f6c32b7efc13b1797c8d078414431f24e
safyali46-boop <safy.ali46@gmail.com>
2026-07-23 21:47:55 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -28,5 +28,8 @@ def webhook():
         return jsonify({"status": "success", "message": "تم إضافة السهم بنجاح"})
     return jsonify({"status": "error"}), 400
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
